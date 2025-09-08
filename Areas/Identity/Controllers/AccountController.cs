92ab6ce5dba05ccf1917e067f597c70669a8af98
saddiqmahmoud <saddiqmahmoud73@gmail.com>
2025-09-08 18:36:08 +0300

    Register Page

---
+++ Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cenima_project.Areas.Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ModelsVM/RegisterVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cenima_project.ModelsVM
+{
+    public class RegisterVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required,DataType(DataType.EmailAddress)]
+        public string Email { get; set; } = string.Empty;
+        [Required,DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+        [Required, DataType(DataType.Password),Compare("Password")]
+        public string ConformPassword { get; set; } = string.Empty;
+        public string? Address { get; set; }
+    }
+}
